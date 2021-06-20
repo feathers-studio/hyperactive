6d280137f2e40df37b4af1a2b7c97f64cb4c28f6
Muthu Kumar <muthukumar@thefeathers.in>
2021-06-21 03:24:36 +0530

    (feat) support TextNode
    Signed-off-by: Muthu Kumar <muthukumar@thefeathers.in>

--- src/Node.ts
+++ src/Node.ts
@@ -1,23 +1,31 @@
 import { Element } from "./elements.ts";
 
-type Falsy = false | "" | 0 | 0n | undefined | null;
+export type Falsy = false | "" | 0 | 0n | undefined | null;
 
-const Falsy = new Set([false, "", 0, 0n, undefined, null]);
+export const Falsy = new Set([false, "", 0, 0n, undefined, null]);
 // deno-lint-ignore no-explicit-any
-const isFalsy = (n: any): n is Falsy => Falsy.has(n);
+export const isFalsy = (n: any): n is Falsy => Falsy.has(n);
 
-type Attr = Record<string, string>;
+export type Attr = Record<string, string>;
+
+export type TextNode = string;
+
+export type Nodeish<Tag extends Element = Element> =
+	| Node<Tag>
+	| TextNode
+	| Falsy;
 
 export class Node<Tag extends Element = Element, Attrs extends Attr = Attr> {
 	constructor(
 		public tag: Tag,
 		public attrs: Attrs,
-		public children: Node[],
+		public children: (Node | TextNode)[],
 	) {}
 }
 
 // deno-lint-ignore no-explicit-any
-const isNode = (n: any): n is Node => n instanceof Node;
+export const isNode = (n: any): n is Node | TextNode =>
+	n instanceof Node || typeof n === "string";
 
 export function h<Tag extends Element = Element, Attrs extends Attr = Attr>(
 	elem: Tag,
@@ -26,25 +34,25 @@ export function h<Tag extends Element = Element, Attrs extends Attr = Attr>(
 
 export function h<Tag extends Element = Element>(
 	elem: Tag,
-	...children: (Node | Falsy)[]
+	...children: Nodeish[]
 ): Node<Tag>;
 
 export function h<Tag extends Element, Attrs extends Attr = Attr>(
 	elem: Tag,
 	props: Attr,
-	...children: Node[]
+	...children: Nodeish[]
 ): Node<Tag>;
 
 export function h<Tag extends Element, Attrs extends Attr = Attr>(
 	elem: Tag,
-	props?: Attrs | Node | Falsy,
-	...children: (Node | Falsy)[]
+	props?: Attrs | Nodeish | Falsy,
+	...children: Nodeish[]
 ): Node<Tag>;
 
 export function h(
 	elem: Element,
-	props?: Attr | Node | Falsy,
-	...childNodes: (Node | Falsy)[]
+	props?: Attr | Nodeish,
+	...childNodes: Nodeish[]
 ): Node {
 	const [attrs, children] =
 		isNode(props) || isFalsy(props)
@@ -60,19 +68,16 @@ export function h(
 	);
 }
 
-type hElement<Tag extends Element = Element> =
+export type hElement<Tag extends Element = Element> =
 	//
-	((props?: Node["attrs"]) => Node<Tag>) &
-		(<Nodeish extends Node | Falsy>(childNode: Nodeish) => Node<Tag>) &
-		((props: Node["attrs"], ...childNodes: (Node | Falsy)[]) => Node<Tag>);
+	((props?: Attr) => Node<Tag>) &
+		(<TNodeish extends Nodeish>(childNode: TNodeish) => Node<Tag>) &
+		((props: Attr, ...childNodes: Nodeish[]) => Node<Tag>);
 
 export const elements = <Elems extends Element[]>(...elems: Elems) => {
 	return elems.map(
 		(elem): hElement<typeof elem> =>
-			(
-				props?: Node["attrs"] | Node | Falsy,
-				...childNodes: (Node | Falsy)[]
-			) =>
+			(props?: Attr | Nodeish, ...childNodes: Nodeish[]) =>
 				h(elem, props, ...childNodes),
 	) as {
 		[Index in keyof Elems]: hElement<
