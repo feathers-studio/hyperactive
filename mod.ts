fbc4ed5478827bc1c5c8225c6ba82cf219897b1b
Muthu Kumar <muthukumar@thefeathers.in>
2021-06-23 09:09:28 +0530

    (refactor) hElement -> hElement.ts
    Signed-off-by: Muthu Kumar <muthukumar@thefeathers.in>

--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
 export * from "./src/elements.ts";
 export * from "./src/Node.ts";
+export * from "./src/hElement.ts";
 export * from "./src/parse.ts";
 export * from "./src/render.ts";
---
+++ src/hElement.ts
@@ -0,0 +1,39 @@
+import { Element, CustomTag } from "./elements.ts";
+import { Nodeish, Attr, Node, h } from "./Node.ts";
+
+export type hElement<Tag extends Element = Element> =
+	//
+	((props?: Attr) => Node<Tag>) &
+		((...childNodes: Nodeish[]) => Node<Tag>) &
+		((props: Attr, ...childNodes: Nodeish[]) => Node<Tag>);
+
+const hElementCache = new Map<Element, hElement>();
+
+function getHElement<Elem extends Element>(element: Elem): hElement<Elem> {
+	const fromCache = hElementCache.get(element);
+	if (fromCache) return fromCache as hElement<Elem>;
+
+	function hElement(props?: Attr | Nodeish, ...childNodes: Nodeish[]) {
+		return h(element, props, ...childNodes);
+	}
+
+	hElementCache.set(element, hElement);
+	return hElement;
+}
+
+export type ElementsToHElements<Elements extends [...Element[]]> = {
+	[Index in keyof Elements]: hElement<
+		// @ts-ignore TypeScript pls
+		Elements[Index]
+	>;
+} & { length: Elements["length"] };
+
+function mapElements<Elements extends Element[]>(...elements: Elements) {
+	return elements.map(getHElement) as ElementsToHElements<Elements>;
+}
+
+export const elements = new Proxy(mapElements, {
+	get<E extends Element>(_: unknown, element: E): hElement<E> {
+		return getHElement(element);
+	},
+}) as typeof mapElements & { [k in Exclude<Element, CustomTag>]: hElement<k> };
