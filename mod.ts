2584900694bfab724045192b81099adb248f7867
Muthu Kumar <muthukumar@thefeathers.in>
2021-06-29 01:21:59 +0530

    (fix) duplicate Element export
    Signed-off-by: Muthu Kumar <muthukumar@thefeathers.in>

--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
-export * from "./src/elements.ts";
+export type { Element as SupportedElements } from "./src/elements.ts";
+export type { Element as DeprecatedElements } from "./src/elements.deprecated.ts";
 export * from "./src/Node.ts";
 export * from "./src/State.ts";
 export * from "./src/hElement.ts";
