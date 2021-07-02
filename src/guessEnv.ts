fbc0bb61232b5d0cb743c22bcb2e1a51081426a0
Darvesh <hello@darve.sh>
2021-07-02 23:48:13 +0530

    fix: move guessEnv out of util.ts with some simplification

---
+++ src/guessEnv.ts
@@ -0,0 +1,14 @@
+//declare is used to avoid ts-ignoring in multiple places
+declare const process: { version: unknown };
+declare const window: { Deno: unknown; document: unknown };
+
+export const guessEnv = () => {
+	if (typeof process !== "undefined" && process.version) return "node";
+	if (typeof window !== "undefined") {
+		if (window.Deno) return "deno";
+		if (window.document) return "browser";
+	}
+	return undefined;
+};
+
+console.log(guessEnv());
--- src/util.ts
+++ src/util.ts
@@ -14,24 +14,3 @@ export type Falsy = false | "" | 0 | 0n | undefined | null;
 export const Falsy = new Set([false, "", 0, 0n, undefined, null]);
 // deno-lint-ignore no-explicit-any
 export const isFalsy = (n: any): n is Falsy => Falsy.has(n);
-
-export const guessEnv = () => {
-	if (
-		//@ts-ignore global
-		typeof process !== "undefined" &&
-		//@ts-ignore global
-		process.versions &&
-		//@ts-ignore global
-		process.versions.node
-	)
-		return "node";
-	//@ts-ignore global
-	if (typeof window !== "undefined") {
-		//@ts-ignore global
-		if (window.Deno && window.Deno.version && window.Deno.version.deno)
-			return "deno";
-		//@ts-ignore global
-		if (window.document) return "browser";
-	}
-	return undefined;
-};
