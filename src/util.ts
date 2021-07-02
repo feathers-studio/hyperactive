a806ade37d5dd311fd2596ff419d96f89c91afb0
Darvesh <hello@darve.sh>
2021-07-02 21:58:45 +0530

    fix: guessEnv

--- src/util.ts
+++ src/util.ts
@@ -16,10 +16,22 @@ export const Falsy = new Set([false, "", 0, 0n, undefined, null]);
 export const isFalsy = (n: any): n is Falsy => Falsy.has(n);
 
 export const guessEnv = () => {
-	if (typeof window === "undefined") {
-		// @ts-ignore process is a node global API
-		if (typeof process === "undefined") return "node";
-		else return undefined;
-	} else if (typeof window.Deno !== "undefined") return "deno";
-	else return "browser";
+	if (
+		//@ts-ignore global
+		typeof process !== "undefined" &&
+		//@ts-ignore global
+		process.versions &&
+		//@ts-ignore global
+		process.versions.node
+	)
+		return "node";
+	//@ts-ignore global
+	if (typeof window !== "undefined") {
+		//@ts-ignore global
+		if (window.Deno && window.Deno.version && window.Deno.version.deno)
+			return "deno";
+		//@ts-ignore global
+		if (window.document) return "browser";
+	}
+	return undefined;
 };
